d9761e68e5837a50d4be296adb34556e7f8ea8e4
Nathan White <nwhite@carleton.edu>
2013-07-08 16:51:24 -0500

    generate_json speed improvements, code cleanup, comments

--- reason_4.0/lib/core/scripts/feeds/generate_json.php
+++ reason_4.0/lib/core/scripts/feeds/generate_json.php
@@ -3,31 +3,23 @@
  * include dependencies
  * TODO: require authentication.
  */
-$reason_session = false;
 include_once( 'reason_header.php' );
-include_once( CARL_UTIL_INC . 'basic/misc.php');
 reason_include_once( 'classes/entity_selector.php' );
-reason_include_once('function_libraries/image_tools.php');
-reason_include_once('classes/object_cache.php');
+reason_include_once( 'function_libraries/image_tools.php' );
+reason_include_once( 'function_libraries/user_functions.php' );
+reason_include_once( 'classes/object_cache.php' );
 
-$start_time = get_microtime();
 /**
- * ReasonJSON is just a stub that you can extend for some JSON-
- * handling goodness.
+ * ReasonJSON is just a stub that you can extend for some JSON-handling goodness.
+ *
+ * @todo implement as an api
+ * @todo require authentication as appropriate
+ * @todo right now we transform the whole set even with caching off - think about this
+ *
+ * @author Andrew Bacon and Nathan White
  **/
 class ReasonJSON
 {
-	var $es;
-	var $_raw_items;
-	var $_caching;
-	var $_items;
-	var $json;
-	var $_num;
-	var $_offset;
-	var $_type;
-	var $_site_id;
-	var $_last_mod;
-
 	/**
 	 * The constructor instantiates an entity and adds some
 	 * defaults.
@@ -36,219 +28,211 @@ class ReasonJSON
 	function __construct($type, $site_id)
 	{
 		if (empty($type) || empty($site_id))
+		{
 			trigger_error("A type or ID was not given for the json generator.", E_ERROR);
-
-		$this->es = new entity_selector($site_id);
-		$this->es->add_type($type);
-		$this->es->set_order('last_modified DESC');
-
+		}
 		$this->type($type);
 		$this->site_id($site_id);
-		$this->last_mod(false);
 	}
 
-	function get_most_recent_date()
+	function get_items_selector()
 	{
-		if (!isset($this->_most_recent)) {
-			$esCopy = carl_clone($this->es);
-			$esCopy->set_num(1);
-			$esCopy->limit_tables();
-			$esCopy->limit_fields('last_modified');
-			$result = $esCopy->run_one();
-			$this->_most_recent = array_shift($result)->get_value('last_modified');
-		}
-
-		return $this->_most_recent;
+		$es = new entity_selector($this->site_id());
+		$es->add_type($this->type());
+		$es->set_order('last_modified DESC');
+		return $es;
 	}
-
-	function type($type = NULL) {
-		if (empty($type))
-			return $this->_type;
-		else
-			$this->_type = $type;
-	}
-	function site_id($site_id = NULL)
+	
+	function get_last_modified_item_selector()
 	{
-		if (empty($site_id))
-			return $this->_site_id;
-		else
-			$this->_site_id = $site_id;
+		$es = new entity_selector($this->site_id());
+		$es->add_type($this->type());
+		$es->limit_tables();
+		$es->limit_fields('last_modified');
+		$es->set_num(1);
+		$es->set_order('last_modified DESC');
+		return $es;
 	}
-
-	function cache($key = NULL, $obj = NULL)
+	
+	final function get_last_modified_item()
 	{
-		if (isset($this->_cache)) {
-			$cache = $this->_cache;
-		} else {
-			$cache = new ReasonObjectCache();
-			$most_recent = $this->get_most_recent_date();
-			if (empty($key) && empty($obj)) {
-			// Retrieve particular cached results
-				if ($this->last_mod() != false && $this->last_mod() != null) {
-					$cacheKey = "jsongen_" . $this->type() . $this->site_id() . $this->last_mod();
-					$cache->init($cacheKey);
-				} else { // Retrieve the most recent cached results
-					$cacheKey = "jsongen_" . $this->type() . $this->site_id() . $most_recent;
-					$cache->init($cacheKey);
-				}
-			} else {
-				$cache->init($key);
-				$cache->set($obj);
-				return;
+		if (!isset($this->_last_modified_item))
+		{
+			$es = $this->get_last_modified_item_selector();
+			if ($result = $es->run_one())
+			{
+				$this->_last_modified_item = array_shift($result);
 			}
-			return $cache->fetch();
+			else $this->_last_modified_item = false;
 		}
+		return $this->_last_modified_item;
 	}
 
-	function last_mod($last_mod = NULL)
+	final function type($type = NULL)
 	{
-		if (empty($last_mod))
-			return $this->_last_mod;
-		else
-			$this->_last_mod = $last_mod;
+		if ($type !== NULL) $this->_type = $type;
+		return $this->_type;
+	}
+	
+	final function site_id($site_id = NULL)
+	{
+		if ($site_id !== NULL) $this->_site_id = $site_id;
+		return $this->_site_id;
 	}
 
-	function _fetch_raw_items() {
-		$this->_raw_items = $this->es->run_one();
+	final function num($num = NULL)
+	{
+		if ($num !== NULL) $this->_num = $num;
+		return $this->_num;
 	}
 
-	function _get_items() {
-		if (!$this->_items) {
-			$this->_fetch_raw_items();
-			$this->_transform_and_count_items();
+	final function offset($offset = NULL)
+	{
+		if ($offset !== NULL) $this->_offset = $offset;
+		return $this->_offset;			
+	}
+	
+	final function caching($caching = NULL)
+	{
+		if (isset($caching)) $this->_caching = $caching;
+		return $this->_caching;
+	}
+	
+	final function get_cache_key()
+	{
+		return 'jsongen_' . $this->type() . '_' . $this->site_id() . '_' . $this->last_mod();
+	}
+	
+	final function cache($obj = NULL)
+	{
+		if ($obj === NULL && isset($this->_cache))
+		{
+			return $this->_cache;
 		}
-		
+		$cache_key = $this->get_cache_key();
+		$cache = new ReasonObjectCache($cache_key);
+		if ($obj !== NULL) // request to cache
+		{
+			$cache->set($obj);
+		}
+		$this->_cache = $cache->fetch($cache_key);
+		return $this->_cache;
+	}
+
+	final function last_mod($last_mod = NULL)
+	{
+		if ($last_mod !== NULL) $this->_last_mod = $last_mod;
+		if ($last_mod === NULL)
+		{
+			if ($item = $this->get_last_modified_item())
+			{
+				$this->_last_mod = $item->get_value('last_modified');
+			}
+		}
+		return $this->_last_mod;
+	}
+
+	final function make_chunk($obj)
+	{
+		$chunk = Array();
+		$chunk['count'] = $obj['count'];
+		$chunk['items'] = isset($obj['items']) ? array_slice($obj['items'], $this->offset(), $this->num()) : null;
+		return $chunk;
+	}
+	
+	final function get_items()
+	{
+		if (!isset($this->_items))
+		{
+			$items = $this->get_items_selector();
+			if ($items = $items->run_one())
+			{
+				$this->_items['count'] = count($items);
+				foreach ($items as $k => $v)
+				{
+					$this->_items['items'][] = $this->transform_item($v);
+				}
+			}
+			else $this->_items['count'] = 0;
+		}	
 		return $this->_items;
 	}
-
-	function num($num = NULL)
+	
+	final function run()
 	{
-		if (empty($num))
-			return $this->_num;
-		else {
-			$this->_num = $num;
+		// if caching is off or the items are not yet in the cache.
+		$items = ($this->caching()) ? $this->cache() : FALSE;
+		if (!$items)
+		{
+			$items = $this->get_items();
+			if ($this->caching()) $this->cache($items);
 		}
-	}
-	function offset($offset = NULL)
-	{
-		if (empty($offset))
-			return $this->_offset;
-		else
-			$this->_offset = $offset;
+		return json_encode($this->make_chunk($items));
 	}
 }
 
 class ReasonImagesJSON extends ReasonJSON
 {
-	function __construct($site, $type)
-	{
-		parent::__construct($site, $type);
-
-		$this->site_id($site);
-		$this->type($type);
-
-		if( !empty($_REQUEST['q']) )
-		{
-			$this->es->add_relation('(entity.name LIKE "%'.addslashes($_REQUEST['q']) . '%"' .
-						      ' OR meta.description LIKE "%' . addslashes($_REQUEST['q']) . '%"'.
-							  ' OR meta.keywords LIKE "%' . addslashes($_REQUEST['q']) . '%"'.
-							  ' OR chunk.content LIKE "%' . addslashes($_REQUEST['q']) . '%"'.
-							  ')');
-		}
-	}
-
-	/**
-	 * After items have been fetched from the db, this function does some
-	 * work to ready them for serialization.
-	 **/
-	function _transform_and_count_items()
-  {
-    $this->_items['count'] = count($this->_raw_items);
-		if (empty($this->_raw_items))
-		  return;
-    else foreach ($this->_raw_items as $k => $v)
-    {
-      $this->_items['items'][] = $this->_transform_item($v);
-		}
-	}
+	//function __construct($site, $type)
+	//{
+	//	parent::__construct($site, $type);
+	//	$this->site_id($site);
+	//	$this->type($type);
+	//	if( !empty($_REQUEST['q']) )
+	//	{
+	//		$this->es->add_relation('(entity.name LIKE "%'.addslashes($_REQUEST['q']) . '%"' .
+	//					      ' OR meta.description LIKE "%' . addslashes($_REQUEST['q']) . '%"'.
+	//						  ' OR meta.keywords LIKE "%' . addslashes($_REQUEST['q']) . '%"'.
+	//						  ' OR chunk.content LIKE "%' . addslashes($_REQUEST['q']) . '%"'.
+	//						  ')');
+	//	}
+	//}
 
 	/**
 	 * This function should be overloaded in each new ReasonJSON type. It is the
-	 * mapping of values from the Reason object to the JSON object.
-	 **/
-  function _transform_item($v)
-  {
-			$newArray = array();
-			$newArray['id'] = $v->get_value('id');
-			$newArray['name'] = $v->get_value('name');
-			$newArray['description'] = $v->get_value('description');
-			$newArray['pubDate'] = $v->get_value('creation_date');
-      $newArray['lastMod'] = $v->get_value('last_modified');
-			$newArray['link'] = $this->make_image_link($newArray['id'], 'standard');
-			$newArray['thumbnail'] = $this->make_image_link($newArray['id'], 'thumbnail');
-      $newArray['content'] = $v->get_value('content');
-      $newArray['keywords'] = $v->get_value('keywords');
-
-			return $newArray;
-  }
-
-
-	function _make_chunk($obj)
-	{
-		$chunk = Array();
-		$chunk['count'] = $obj['count'];
-		$chunk['items'] = array_slice($obj['items'], $this->offset(), $this->num());
-		return $chunk;
-	}
-
-	function make_image_link($id, $size=null)
-	{
-		$filename = reason_get_image_filename($id, $size);
-		return 'http://' . REASON_HOST.WEB_PHOTOSTOCK. $filename;
-	}
-
-	function caching($caching = NULL) {
-		if (isset($caching))
-			$this->_caching = $caching;
-		else
-			return $this->_caching;
-	}
-
-	function run()
-	{
-		$cached_results = $this->cache();
-		if (!$cached_results || !$this->caching()) {
-			$this->es->set_order( 'entity.last_modified DESC, dated.datetime DESC, entity.name ASC' );
-			$this->_get_items();
-			$cacheKey = "jsongen_" . $this->type() . $this->site_id() . ($this->last_mod() ? $this->last_mod() : $this->get_most_recent_date());
-			$this->cache($cacheKey, $this->_items);
-		}
-		return json_encode($this->_make_chunk($this->cache()));
+	 * mapping of values from the Reason entities to the JSON object.
+	 */
+	function transform_item($v)
+	 {
+		$newArray = array();
+		$newArray['id'] = $v->get_value('id');
+		$newArray['name'] = $v->get_value('name');
+		$newArray['description'] = $v->get_value('description');
+		$newArray['pubDate'] = $v->get_value('creation_date');
+		$newArray['lastMod'] = $v->get_value('last_modified');
+		$newArray['link'] = 'http://' . REASON_HOST.WEB_PHOTOSTOCK . reason_format_image_filename($v->id(), $v->get_value('image_type'), 'standard');
+		$newArray['thumbnail'] = 'http://' . REASON_HOST.WEB_PHOTOSTOCK . reason_format_image_filename($v->id(), $v->get_value('thumbnail_image_type'), 'thumbnail');
+		$newArray['content'] = $v->get_value('content');
+		$newArray['keywords'] = $v->get_value('keywords');
+		return $newArray;
 	}
 }
 
-
-if (isset($_GET['type']) && isset($_GET['site_id'])) {
-		$type = turn_into_string($_GET['type']);
-		$site_id = turn_into_int($_GET['site_id']);
-		$last_mod = (isset($_GET['lastmod'])) ? $_GET['lastmod'] : false;
-		if (id_of($type) == id_of('image')) {
-				$reasonImagesJson = new ReasonImagesJSON(id_of($type), $site_id);
-				$num = !empty($_REQUEST['num']) ? turn_into_int($_REQUEST['num']) : '500';
-				$offset = !empty($_REQUEST['offset']) ? turn_into_int($_REQUEST['offset']) : '0';
-				$reasonImagesJson->num($num);
-				$reasonImagesJson->offset($offset);
-				$reasonImagesJson->last_mod($last_mod);
-				$reasonImagesJson->caching((isset($_GET['caching']))? turn_into_boolean($_GET['caching']) : true);
-				print($reasonImagesJson->run());
-		}
-} else
+if (!reason_check_authentication())
+{
+	http_response_code(403);
+	echo json_encode(array("error" => 403));
+}
+elseif (isset($_GET['type']) && isset($_GET['site_id']))
+{
+	$type = turn_into_string($_GET['type']);
+	$site_id = turn_into_int($_GET['site_id']);
+	$last_mod = (isset($_GET['lastmod'])) ? $_GET['lastmod'] : false;
+	if (id_of($type) == id_of('image'))
+	{
+		$reasonImagesJson = new ReasonImagesJSON(id_of($type), $site_id);
+		$num = !empty($_REQUEST['num']) ? turn_into_int($_REQUEST['num']) : '500';
+		$offset = !empty($_REQUEST['offset']) ? turn_into_int($_REQUEST['offset']) : '0';
+		$reasonImagesJson->num($num);
+		$reasonImagesJson->offset($offset);
+		$reasonImagesJson->last_mod($last_mod);
+		$reasonImagesJson->caching((isset($_GET['caching']))? turn_into_boolean($_GET['caching']) : true);
+		print($reasonImagesJson->run());
+	}
+} 
+else
 {
 	http_response_code(400);
 	echo json_encode(array("error" => 400));
 }
-
-reason_log_page_generation_time( round( 1000 * (get_microtime() - $start_time) ) );
-
-
 ?>
